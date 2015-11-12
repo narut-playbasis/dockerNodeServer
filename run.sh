43118fde1b41a7cadc6a0a6e5d27a1754c46023a
Narut Tripopnat <narut.t@playbasis.com>
2015-11-12 15:35:54 +0700

    add initial action

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-# declare STRING variable
-STRING="Hello World"
-#print variable on a screen
-echo $STRING
+if ! type "docker-compose" > /dev/null; then
+  # install docker-compose here
+  sudo wget --no-check-certificate https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m`
+  sudo mv docker-compose-`uname -s`-`uname -m` /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  docker-compose --version
+fi
+
+docker pull narut/nodeserver
+docker pull redis
+docker-compose up
